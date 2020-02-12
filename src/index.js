60f5755f69656c7adc73acc84fbcbf9a26db85ed
WmOnTheWay <1293822641@qq.com>
2020-02-12 20:49:32 +0800

    ok

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import rootReducer from './reducers'
-import App from './components/App';
+import rootReducer from './todoApp/reducers'
+import App from './todoApp/components/App';
 import * as serviceWorker from './serviceWorker';
+import 'babel-polyfill'
+
+import Root from './redditApi/containers/Root'
 
 const store = createStore(rootReducer);
+const Jsx =()=> (
+    <div>
+        <Provider store={store}>
+            <App />
+        </Provider>
+        <Root />
+    </div>
+)
 
-ReactDOM.render(<Provider store={store}>
-    <App/>
-</Provider>, document.getElementById('root'))
+
+ReactDOM.render(<Jsx />, document.getElementById('root'))
 
 
 serviceWorker.unregister();
