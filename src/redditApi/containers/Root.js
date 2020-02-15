935e071eee5d1ac742b91965279a7674b941c0b1
WmOnTheWay <1293822641@qq.com>
2020-02-15 16:25:30 +0800

    .

--- src/redditApi/containers/Root.js
+++ src/redditApi/containers/Root.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import AsyncApp from './AsyncApp'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { createLogger } from 'redux-logger'
 import rootReducer from '../ARS/reducers'
 import createSagaMiddleware from 'redux-saga'
@@ -18,10 +18,20 @@ const preloadedState = {
   selectedSubreddit: "reactjs",
   viewSignlQuestion: {}
 }
+const composeEnhancers =
+  typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+       // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+const enhancer = composeEnhancers(
+  applyMiddleware(sagaMiddleware, loggerMiddleware),
+  // other store enhancers if any
+);
 const store = createStore(
   rootReducer,
   preloadedState,
-  applyMiddleware(sagaMiddleware, loggerMiddleware)
+  enhancer
 )
 sagaMiddleware.run(watchForPostReddit)
 export default class Root extends Component {
